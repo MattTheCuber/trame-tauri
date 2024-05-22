90bb42b2995f6b3c41404e0fc6c7708da718ffb6
Matthew Vine <mvine@compotechinc.com>
2024-05-22 09:57:06 -0400

    feat(example 4): add some more feature demonstrations

--- examples/04_http_ws_multi_windows/server2.py
+++ examples/04_http_ws_multi_windows/server2.py
@@ -1,7 +1,8 @@
 from trame.app import get_server
 from trame.decorators import TrameApp
 from trame.ui.vuetify3 import SinglePageLayout
-from trame.widgets import html, tauri, vuetify3 as v3
+from trame.widgets import html, tauri
+from trame.widgets import vuetify3 as v3
 
 
 @TrameApp()
@@ -48,6 +49,7 @@ class TestApp:
                                     "{{ position }} | {{ size }} | {{ scaleFactor }}"
                                 )
 
+                html.Div("URL: {{ window.location }}")
                 html.Div("p({{ position }}) - s({{ size }}) - d({{ scale }})")
                 with tauri.Window(
                     url="http://localhost:4444/index.html?ui=hello_world",
@@ -55,11 +57,35 @@ class TestApp:
                     title="Hello",
                     width=300,
                     height=300,
+                    X=100,
+                    y=100,
+                    options=(
+                        {
+                            "alwaysOnTop": True,  # Does not work
+                            "center": True,  # Does not work
+                            "closable": False,  # Does not work
+                            "decorations": False,  # Does not work
+                            "focus": False,  # Does not work
+                            "minHeight": 200,
+                            "minWidth": 200,
+                            "maxHeight": 800,
+                            "maxWidth": 800,
+                            "maximizable": False,  # Does not work
+                            "minimizable": False,  # Does not work
+                            "skipTaskbar": True,  # Does not work
+                            "theme": "light",
+                            "fileDropEnabled": False,  # Does not work
+                        },
+                    ),
+                    prevent_close=True,  # Does not work
                     moved="position = $event",
                     resized="size = $event",
                     scale_changed="scale = $event",
                     created="{ position, size, scaleFactor: scale } = $event",
                     closed="window_hello_world = false",
+                    file_drop="console.log('file:', $event)",
+                    focus_changed="console.log('focus:', $event)",
+                    theme_changed="console.log('theme:', $event)",
                 ) as w:
                     with v3.Template(raw_attrs=['v-slot="data"']):
                         v3.VBtn("Center", click=w.center)
@@ -70,6 +96,15 @@ class TestApp:
                         v3.VBtn("Minimize", click=w.minimize)
                         v3.VBtn("Un-Minimize", click=w.unminimize)
                         v3.VBtn("Focus", click=w.grab_focus)
+                        v3.VBtn("Fullscreen On", click=lambda: w.set_fullscreen(True))
+                        v3.VBtn("Fullscreen Off", click=lambda: w.set_fullscreen(False))
+                        v3.VBtn(
+                            "Request User's Attention",
+                            click=lambda: w.request_user_attention(2),
+                        )
+                        v3.VBtn("Set Position", click=lambda: w.set_position(0, 0))
+                        v3.VBtn("Set Size", click=lambda: w.set_size(400, 400))
+                        v3.VBtn("Set Title", click=lambda: w.set_title("World"))
                         html.Div("{{ data }}")
 
     def build_ui_hello_world(self):
